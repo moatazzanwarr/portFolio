8cd3b38ac98206176f2a4eadd28f9f93e0fa7bf0
moatazzanwarr <moatazzanwarr@gmail.com>
2023-03-16 14:30:35 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -8,4 +8,22 @@ function open_menu(){
 
 function close_menu(){
     links.style.display = "none";
+};
+
+function check(){
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+
+    if(name.value.length < 10){
+        name.style.borderColor = "red";
+        return false;
+    }
+    else if(email.value.indexOf("@") == -1 || email.value.length < 10){
+        name.style.borderColor = "#c850c0";
+        email.style.borderColor = "red";
+        return false;
+    }
+    else{
+        email.style.borderColor = "#c850c0"
+    }
 }
